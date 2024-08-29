083af04d0d0c4e8ed6cc19bc70e281f085506abb
Muhammad Adnan <dononlineboss@gmail.com>
2024-08-29 23:30:06 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Welcome to My Portfolio</h2>
+        <p>This is where I showcase my work and skills.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
