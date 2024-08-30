831a3107755302c2006aa2a58bea65fac13f29a4
Muhammad Adnan <madnanedu@gmail.com>
2024-08-30 12:22:25 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-        <h1>Welcome to My Portfolio</h1>
+        <h1>Welcome to My Portfolio.</h1>
         <p>This is where I showcase my work and skills.</p>
     </div>
   );
