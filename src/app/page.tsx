0b39260f760490389789688b5e9d4cdde866455b
madnan-github <dononlineboss@gmail.com>
2024-09-03 08:59:35 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is where I showcase my work and skills</p>
+        <h1>Welcome to My Portfolio.</h1>
+        <p>This is where I showcase my work and skills.</p>
     </div>
   );
 }
